9592779c22d939b0da9a640cb66a8695d0a35d7b
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-31 08:19:46 +0100

    Set up server socket and client socket

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 const app = express();
 
 var server = app.listen(3000, () => {
@@ -7,4 +8,13 @@ var server = app.listen(3000, () => {
 
 //Run app, then load http://localhost:3000 in a browser to see the output.
 
+//Static files
 app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+// Server listen for connection
+io.on('connection', (socket) => {
+	console.log('Made socket connection', socket.id);
+});
---
+++ public/scripts/chat.js
@@ -0,0 +1,2 @@
+// Make client connection
+var socket = io.connect('http://localhost:3000');
